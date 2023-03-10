11dcbb2ad75a281044b7c7ba8a331ce3e29ce6cb
Pasha A <pashakun@gmail.com>
2023-03-10 15:32:18 +0900

    Add files via upload

---
+++ contracts/StartNotary.sol
@@ -0,0 +1,20 @@
+//pragma solidity >=0.8.19;
+pragma solidity >=0.5.1;
+
+contract StarNotary {
+
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor() public {
+        starName = "Awesome Udacity Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+
+}
